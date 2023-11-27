70d7ede0f0315f085eec3bfe3f9d7070248e95ce
Tampanc78 <91440906+Tampanc78@users.noreply.github.com>
2023-11-27 17:50:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Fungsi untuk menyimpan data pengguna ke dalam file CSV
-def save_data_to_csv(data, username):
-    data.to_csv(f'{username}_data.csv', index=False)
+def save_data_to_csv(data):
+    data.to_csv('user_data.csv', index=False)
 
-# Fungsi untuk membaca data pengguna dari file CSV
-@st.cache(allow_output_mutation=True)
-def read_data_from_csv(username):
+def read_data_from_csv():
     try:
-        return pd.read_csv(f'{username}_data.csv')
+        return pd.read_csv('user_data.csv')
     except FileNotFoundError:
         return pd.DataFrame(columns=['Username', 'Pemasukan', 'Persentase_Tabungan', 'Tanggal_Pengeluaran', 'Jumlah_Pengeluaran', 'Jenis_Pengeluaran'])
 
-# Fungsi untuk menyimpan data pengguna
 def save_data(username, pemasukan, persentase_tabungan, tanggal_pengeluaran, jumlah_pengeluaran, jenis_pengeluaran):
     data = pd.DataFrame({
         'Username': [username],
@@ -24,37 +20,26 @@ def save_data(username, pemasukan, persentase_tabungan, tanggal_pengeluaran, jum
         'Jumlah_Pengeluaran': [jumlah_pengeluaran],
         'Jenis_Pengeluaran': [jenis_pengeluaran]
     })
-
-    if username in st.session_state.user_data:
-        st.session_state.user_data[username] = pd.concat([st.session_state.user_data[username], data], ignore_index=True)
-    else:
-        st.session_state.user_data[username] = data
-
     return data
 
-# Fungsi untuk menampilkan halaman login
 def show_login_page():
     username = st.text_input("Masukkan Nama Anda:")
     if st.button("Masuk"):
-        if 'user_data' in st.session_state and username in st.session_state.user_data:
-            st.success(f"Selamat datang kembali, {username}!")
+        if 'data' in st.session_state and not st.session_state.data.empty:
+            if username in st.session_state.data['Username'].values:
+                st.success(f"Selamat datang kembali, {username}!")
+            else:
+                st.success(f"Selamat datang, {username}!")
         else:
             st.success(f"Selamat datang, {username}!")
         return True, username
     return False, None
 
-# Fungsi untuk menampilkan halaman tabungan
 def show_tabungan_page():
     st.header("Tabungan")
     pemasukan = st.number_input("Pendapatan Bulanan (Rp):", min_value=0)
-    
-    # Pilihan persentase tabungan
-    persentase_options = [15, 20, 25, "Lainnya"]
-    persentase_tabungan = st.selectbox("Persentase Tabungan:", persentase_options)
 
-    if persentase_tabungan == "Lainnya":
-        persentase_tabungan = st.number_input("Masukkan Persentase Tabungan (0-100):", min_value=0, max_value=100)
-
+    persentase_tabungan = st.selectbox("Persentase Tabungan:", [15, 20, 25])
     tabungan_amount = (persentase_tabungan / 100) * pemasukan
     sisa_uang = pemasukan - tabungan_amount
     
@@ -65,10 +50,8 @@ def show_tabungan_page():
         st.session_state.pemasukan = pemasukan
         st.session_state.persentase_tabungan = persentase_tabungan
 
-        # Tambahkan pesan notifikasi bahwa data tabungan telah berhasil disimpan
-        st.success("Data tabungan Anda telah berhasil disimpan!")
+        st.success("tabungan tersimpan, jangan boros boros ya!")
 
-# Fungsi untuk menampilkan halaman pengeluaran
 def show_pengeluaran_page():
     st.header("Pengeluaran")
     tanggal_pengeluaran = st.date_input("Tanggal Pengeluaran:")
@@ -84,75 +67,62 @@ def show_pengeluaran_page():
             jumlah_pengeluaran,
             jenis_pengeluaran
         )
-        if 'user_data' in st.session_state:
-            if st.session_state.username in st.session_state.user_data:
-                st.session_state.user_data[st.session_state.username] = pd.concat([st.session_state.user_data[st.session_state.username], data], ignore_index=True)
-            else:
-                st.session_state.user_data[st.session_state.username] = data
+        if 'data' in st.session_state:
+            st.session_state.data = pd.concat([st.session_state.data, data], ignore_index=True)
         else:
-            st.session_state.user_data = {st.session_state.username: data}
+            st.session_state.data = data
 
-        # Tambahkan pesan notifikasi bahwa data pengeluaran telah berhasil disimpan
-        st.success("Data pengeluaran Anda telah berhasil disimpan!")
+        st.success("Pengeluaran sudah dicatat, jangan boros ya!")
 
-# Fungsi untuk menampilkan halaman ringkasan keuangan
-def show_ringkasan_page(username):
-    if username in st.session_state.user_data:
-        data = st.session_state.user_data[username]
-        st.write(data)
-        category_sum = data.groupby('Jenis_Pengeluaran')['Jumlah_Pengeluaran'].sum().reset_index()
-        fig = px.pie(category_sum, values='Jumlah_Pengeluaran', names='Jenis_Pengeluaran', title='Ringkasan Pengeluaran')
-        st.plotly_chart(fig)
-        pemasukan = data['Pemasukan'].iloc[0]
-        persentase_tabungan = data['Persentase_Tabungan'].iloc[0]
-        total_pengeluaran = data['Jumlah_Pengeluaran'].sum()
-        sisa_uang = pemasukan - total_pengeluaran
-        tabungan_amount = (persentase_tabungan / 100) * pemasukan
+def show_ringkasan_page(data):
+    st.write(data)
+    category_sum = data.groupby('Jenis_Pengeluaran')['Jumlah_Pengeluaran'].sum().reset_index()
+    fig = px.pie(category_sum, values='Jumlah_Pengeluaran', names='Jenis_Pengeluaran', title='Ringkasan Pengeluaran')
+    st.plotly_chart(fig)
+    pemasukan = data['Pemasukan'].iloc[0]
+    persentase_tabungan = data['Persentase_Tabungan'].iloc[0]
+    total_pengeluaran = data['Jumlah_Pengeluaran'].sum()
+    sisa_uang = pemasukan - total_pengeluaran
+    tabungan_amount = (persentase_tabungan / 100) * pemasukan
 
-        if total_pengeluaran > pemasukan:
-            st.warning("Uang Anda Habis!")
-        elif total_pengeluaran > sisa_uang:
-            st.warning("Uang Anda Tidak Cukup!")
-        else:
-            sisa_setelah_pengeluaran = sisa_uang - total_pengeluaran
-            tabungan_amount += sisa_setelah_pengeluaran
-
-            st.success(f"Sisa Uang Anda: Rp {sisa_setelah_pengeluaran}")
-            st.success(f"Total Tabungan: Rp {tabungan_amount}")
-
-            if st.button("Finish"):
-                st.balloons()
+    if total_pengeluaran > pemasukan:
+        st.warning("Uang Anda Habis!")
+    elif total_pengeluaran > sisa_uang:
+        st.warning("Uang Anda Tidak Cukup!")
     else:
-        st.warning("Data untuk pengguna ini tidak ditemukan.")
+        sisa_setelah_pengeluaran = sisa_uang - total_pengeluaran
+        tabungan_amount += sisa_setelah_pengeluaran
 
-# Fungsi untuk menampilkan halaman pengaturan
-def show_settings_page(username):
-    if username in st.session_state.user_data:
-        st.write(f"Data yang telah disimpan untuk pengguna {username}:")
-        st.dataframe(st.session_state.user_data[username], height=200)
-        selected_rows = st.multiselect("Pilih Baris", st.session_state.user_data[username].index)
+        st.success(f"Sisa Uang Anda: Rp {sisa_setelah_pengeluaran}")
+        st.success(f"Total Tabungan: Rp {tabungan_amount}")
 
-        if st.button("Hapus Data Terpilih"):
-            selected_data = st.session_state.user_data[username][st.session_state.user_data[username].index.isin(selected_rows)]
-            st.write("Data yang akan dihapus:")
-            st.write(selected_data)
+        if st.button("Finish"):
+            st.balloons()
 
-            st.session_state.user_data[username] = st.session_state.user_data[username][~st.session_state.user_data[username].index.isin(selected_rows)]
-            st.success("Data terpilih telah dihapus!")
+def show_settings_page(data):
+    st.write("Data yang telah disimpan:")
 
-        if st.button("Hapus Semua Data"):
-            st.session_state.user_data[username] = pd.DataFrame(columns=['Username', 'Pemasukan', 'Persentase_Tabungan', 'Tanggal_Pengeluaran', 'Jumlah_Pengeluaran', 'Jenis_Pengeluaran'])
-            st.success("Semua data telah dihapus!")
-    else:
-        st.warning("Data untuk pengguna ini tidak ditemukan.")
+    st.dataframe(data, height=200)
 
-# Fungsi utama
+    selected_rows = st.multiselect("Pilih Baris", data.index)
+
+    if st.button("Hapus Data Terpilih"):
+        selected_data = data[data.index.isin(selected_rows)]
+        st.write("Data yang akan dihapus:")
+        st.write(selected_data)
+
+        data = data[~data.index.isin(selected_rows)]
+        st.session_state.data = data
+        st.success("Data terpilih telah dihapus!")
+
+    if st.button("Hapus Semua Data"):
+        st.session_state.data = pd.DataFrame(columns=['Username', 'Pemasukan', 'Persentase_Tabungan', 'Tanggal_Pengeluaran', 'Jumlah_Pengeluaran', 'Jenis_Pengeluaran'])
+        st.success("Semua data telah dihapus!")
+
 def main():
-    # Buat session state jika belum ada
-    if 'user_data' not in st.session_state:
-        st.session_state.user_data = {}
+    if 'data' not in st.session_state:
+        st.session_state.data = read_data_from_csv()
 
-    # Tampilkan "YOURBOOK" di sidebar dengan ukuran 4x lebih besar
     st.sidebar.markdown('<h1 style="text-align: center; color: #ffffff;">YOURBOOK</h1>', unsafe_allow_html=True)
 
     page = st.sidebar.selectbox("Halaman", ["Pengguna", "Tabungan", "Pengeluaran", "Ringkasan", "Pengaturan"])
@@ -160,7 +130,6 @@ def main():
     if page == "Pengguna":
         logged_in, username = show_login_page()
         if logged_in:
-            # Simpan data pengguna
             st.session_state.username = username
             st.sidebar.text(f"Akun: {st.session_state.username}")
 
@@ -173,16 +142,13 @@ def main():
             show_pengeluaran_page()
 
     elif page == "Ringkasan":
-        if hasattr(st.session_state, 'username') and st.session_state.username in st.session_state.user_data:
-            show_ringkasan_page(st.session_state.username)
+        if hasattr(st.session_state, 'data'):
+            show_ringkasan_page(st.session_state.data)
 
     elif page == "Pengaturan":
-        if hasattr(st.session_state, 'username') and st.session_state.username in st.session_state.user_data:
-            show_settings_page(st.session_state.username)
+        show_settings_page(st.session_state.data)
 
-    # Simpan data ke dalam file CSV setiap kali aplikasi dijalankan
-    for username, data in st.session_state.user_data.items():
-        save_data_to_csv(data, username)
+    save_data_to_csv(st.session_state.data)
 
 if __name__ == "__main__":
     main()
